048de17e0de64a0ccb7394e38f5f80e1225cac5f
Heloá <heloa.jennyffer@academico.ifpb.edu.br>
2026-04-15 11:12:36 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image(loh.jpeg)
+st.image("loh.jpeg")
 st.write("Site Heloá")
 st.link_button("Acessar", "https://sites.google.com/d/1I86KKTtO5lHEQ5UCQbhZElQLa4axHyxL/p/1VBaw2kBCL66JRltCMmyUHavoiOfeF_4_/edit")
