edbabf97b4c0636e8f592d798aefe6f060c5b1d9
Heloá <heloa.jennyffer@academico.ifpb.edu.br>
2026-04-15 11:07:39 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2025/09/anavitoria-album-surpresa.png?w=740")
+st.image(loh.png)
 st.write("Site Heloá")
-st.link_button("Acessar", "https://www.youtube.com/playlist?list=OLAK5uy_m-oAvxplq0RhODVr4YePmho24LVbnc3j4")
+st.link_button("Acessar", "https://sites.google.com/d/1I86KKTtO5lHEQ5UCQbhZElQLa4axHyxL/p/1VBaw2kBCL66JRltCMmyUHavoiOfeF_4_/edit")
