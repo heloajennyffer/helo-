ebbe290f36474febb50606aa39ea9ed795dc9fd1
Heloá <heloa.jennyffer@academico.ifpb.edu.br>
2026-04-15 11:06:18 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.image("https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2025/09/anavitoria-album-surpresa.png?w=740")
+st.write("Site Heloá")
+st.link_button("Acessar", "https://www.youtube.com/playlist?list=OLAK5uy_m-oAvxplq0RhODVr4YePmho24LVbnc3j4")
