f579f05dd03391011c39a369c9bc447261f889c4
Heloá <heloa.jennyffer@academico.ifpb.edu.br>
2026-05-13 12:02:56 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.image("loh.jpeg")
 st.write("Site Heloá")
 st.link_button("Acessar", "https://sites.google.com/d/1I86KKTtO5lHEQ5UCQbhZElQLa4axHyxL/p/1VBaw2kBCL66JRltCMmyUHavoiOfeF_4_/edit")
+st.image("apple.png")
+st.write("Apple é uma empresa multinacional norte-americana que tem o objetivo de projetar e comercializar produtos eletrônicos de consumo, software de computador e computadores pessoais. Os produtos de hardware mais conhecidos da empresa incluem a linha de computadores Macintosh, iPod, iPhone, iPad, Apple TV e o Apple Watch.")
+st.image("wpp.png")
